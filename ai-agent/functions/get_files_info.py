49ac4a160710466138914c11d313fe00cd8ace9c
Angel Vasilev <angavasilev@gmail.com>
2025-09-16 23:47:14 +0300

    get file content and tests

--- ai-agent/functions/get_files_info.py
+++ ai-agent/functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from config import MAX_CHARS
 
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
@@ -14,7 +15,8 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
 
     result = list()
 
-    result.append("Result for current directory:")
+    dir = "current" if directory == "." else f"'{directory}'"
+    result.append(f"Result for {dir} directory:")
     for entry in dir_list:
         entry_path = os.path.join(abs_path, entry)
         if os.path.isfile(entry_path):
@@ -36,3 +38,21 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
             result.append(f"- {entry}: file_size={sum} bytes, is_dir=True")
 
     return "\n".join(result)
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    path = os.path.join(working_directory, file_path)
+    abs_path = os.path.abspath(path)
+    rel_path_dir = os.path.relpath(path)
+    if rel_path_dir not in abs_path:
+        return f"Error: cannot read '{
+            file_path
+        }' as it is outside the permitted working directory"
+
+    if not os.path.isfile(abs_path):
+        return f"Error: File not found or is not a regular file: '{file_path}'"
+
+    with open(abs_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+
+        return file_content_string
--- ai-agent/tests.py
+++ ai-agent/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content
 
 
 class TestFunctions(unittest.TestCase):
@@ -12,9 +13,34 @@ class TestFunctions(unittest.TestCase):
 
     def test_calculator_pkg_dir(self):
         result = get_files_info("calculator", "pkg")
-        self.assertTrue("Result for current directory:" in result)
+        self.assertTrue("Result for 'pkg' directory:" in result)
         self.assertTrue("- calculator.py: file_size=1739 bytes, is_dir=False" in result)
         self.assertTrue("- render.py: file_size=768 bytes, is_dir=False" in result)
+
+    def test_calculator_bin_dir(self):
+        result = get_files_info("calculator", "/bin")
+        self.assertTrue("Error: cannot list" in result)
+
+    def test_calculator_main_file(self):
+        result = get_file_content("calculator", "main.py")
+        self.assertTrue("Result for current directory:" in result)
+        self.assertTrue("- main.py: file_size=576 bytes, is_dir=False" in result)
+        self.assertTrue("- tests.py: file_size=1343 bytes, is_dir=False" in result)
+        self.assertTrue("- pkg: file_size=2507 bytes, is_dir=True" in result)
+
+    def test_calculator_pkg_calculator_file(self):
+        result = get_file_content("calculator", "pkg/calculator.py")
+        self.assertTrue("Result for 'pkg' directory:" in result)
+        self.assertTrue("- calculator.py: file_size=1739 bytes, is_dir=False" in result)
+        self.assertTrue("- render.py: file_size=768 bytes, is_dir=False" in result)
+
+    def test_calculator_bin_cat(self):
+        result = get_file_content("calculator", "/bin/cat")
+        self.assertTrue("Error: file not found" in result)
+
+    def test_calculator_does_not_exist_file(self):
+        result = get_file_content("calculator", "pkg/does_not_exist.py")
+        self.assertTrue("Error: file not found" in result)
 
 
 if __name__ == "__main__":
